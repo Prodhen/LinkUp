14578cb624e7c2b9dcf5bba06c19bcb142803578
Arosh <mdaroshprodhen8@gmail.com>
2025-04-03 11:07:14 +0600

    AddAppUserEntity

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc7694584536e20ca4f7404f959fed382f0faed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8d23073a6ddd96ab5970c83f26f1e955456d80d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
