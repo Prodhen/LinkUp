ecfe69a3612b80b3059b2b1974beb32df8586f28
Arosh <mdaroshprodhen8@gmail.com>
2025-04-04 10:01:14 +0600

    introduceDto

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,30 +2,41 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController:BaseApiController
+public class AccountController : BaseApiController
 {
     private readonly DataContext _context;
-    public AccountController(DataContext context){
+    public AccountController(DataContext context)
+    {
         this._context = context;
 
     }
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(string username,string password){
-        using var hmac=new HMACSHA512();
-        var user= new AppUser{
-            UserName=username,
-            PassWordHash=hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
-            PassWordSalt=hmac.Key
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+        if(await UserExists(registerDto.UserName)) return BadRequest("User name is taken already");
+        using var hmac = new HMACSHA512();
+        var user = new AppUser
+        {
+            UserName = registerDto.UserName,
+            PassWordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+            PassWordSalt = hmac.Key
         };
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
 
         return user;
+
+    }
+    private async Task<bool> UserExists(string userName)
+    {
+        return await _context.Users.AnyAsync(x => x.UserName.ToLower().Trim() == userName.ToLower().Trim());
 
     }
 
