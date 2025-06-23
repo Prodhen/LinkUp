82cb055a2f970fbe3bc7200e93f6e9705e91ada9
Arosh <mdaroshprodhen8@gmail.com>
2025-06-24 04:06:14 +0600

    RouterInCode

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,25 +3,26 @@ import { FormsModule } from '@angular/forms'
 import { AccountService } from '../_service/account.service';
 import { NgIf } from '@angular/common';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [FormsModule, BsDropdownModule, RouterLink,RouterLinkActive],
+  imports: [FormsModule, BsDropdownModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
   model: any = {};
   login() {
 
     console.log(this.model);
     this.accountService.login(this.model).subscribe(
       {
-        next: response => {
-          console.log(response);
+        next: _ => {
+          this.router.navigateByUrl('/members');
 
         },
         error: error => console.log(error)
@@ -30,6 +31,7 @@ export class NavComponent {
   }
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
