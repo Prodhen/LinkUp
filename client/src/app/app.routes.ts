b4ca9f8978fc8e613c64e1db7ede3477c838ab0e
Arosh <mdaroshprodhen8@gmail.com>
2025-06-28 13:09:49 +0600

    Adding an Angular route guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { MemberListComponent } from './member/member-list/member-list.component'
 import { MemberDetailsComponent } from './member/member-details/member-details.component';
 import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
-    {path: '',component:HomeComponent},
-    {path: 'members',component:MemberListComponent},
-    {path: 'members/:id',component:MemberDetailsComponent},
-    {path: 'lists',component:ListsComponent},
-    {path: 'messages',component:MessagesComponent},
-    {path: '**',component:HomeComponent,pathMatch: 'full'},
+    { path: '', component: HomeComponent },
+    { path: 'members', component: MemberListComponent, canActivate: [authGuard] },
+    { path: 'members/:id', component: MemberDetailsComponent },
+    { path: 'lists', component: ListsComponent },
+    { path: 'messages', component: MessagesComponent },
+    { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
