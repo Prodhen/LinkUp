6459862c3e02d2bb5d485a74c09a38ac3e0a6d01
Arosh <mdaroshprodhen8@gmail.com>
2025-07-05 23:31:49 +0600

    jwt interceptor

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -6,11 +6,12 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr'
 import { errorsInterceptor } from './_interceptors/errors.interceptor';
+import { jwtInterceptor } from './_interceptors/jwt.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
   provideRouter(routes),
-  provideHttpClient(withInterceptors([errorsInterceptor])),
+  provideHttpClient(withInterceptors([errorsInterceptor, jwtInterceptor])),
   provideAnimations(),
   provideToastr({
     positionClass: 'toast-bottom-right'
--- client/src/app/member/member-details/member-details.component.ts
+++ client/src/app/member/member-details/member-details.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-member-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './member-details.component.html',
   styleUrl: './member-details.component.css'
 })
-export class MemberDetailsComponent {
+export class MemberDetailsComponent implements OnInit {
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  member?: Member;
+  ngOnInit(): void {
+    this.loadMember();
+
+  }
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+
+    this.memberService.getMember(username).subscribe({
+      next: member => this.member = member
+    });
+  }
 
 }
