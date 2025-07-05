54480c2b8dd94ac02205fdfe9a36ba14fc433dec
Arosh <mdaroshprodhen8@gmail.com>
2025-07-05 19:17:26 +0600

    Retrieving the list of members

---
+++ client/src/app/_models/member.ts
@@ -0,0 +1,15 @@
+import { Photo } from "./Photo"
+
+export interface Member {
+    id: number
+    userName: string
+    age: number
+    photoUrl: string
+    knownAs: string
+    created: Date
+    lastActive: Date
+    gender: string
+    city: string
+    country: string
+    photos: Photo[]
+}
