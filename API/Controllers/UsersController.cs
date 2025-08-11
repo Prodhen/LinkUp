614fb36285514f196400b02eaf9e9283390165bf
TN-LAP-0182\TechnoNext <arosh.prodhen@technonext.com>
2025-08-11 23:40:06 +0600

    ChangeMessageOfResponse

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -71,7 +71,7 @@ namespace API.Controllers
                 return NoContent();
             }
 
-            return BadRequest("Failed to update the user");
+            return BadRequest("Failed to update the user due to internal error");
         }
 
     }
