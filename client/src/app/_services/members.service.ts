96218c6ae987a864a94ce725cc095c549111c247
Arosh <mdaroshprodhen8@gmail.com>
2025-08-12 23:45:47 +0600

    cachingData

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { inject, Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../environments/environment.development';
 import { Member } from '../_models/member';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +10,43 @@ import { Member } from '../_models/member';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
+  members = signal<Member[]>([]);
 
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    if (this.members().length > 0) {
+
+      return of(this.members());
+    }
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      tap(members => {
+
+        this.members.set(members);
+      })
+    );
+
   }
 
   getMember(username: string) {
+
+    const member = this.members().find(x => x.userName === username);
+
+
+    if (member) {
+
+      return of(member);
+    }
+
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
-updateMember(member: Member) {
-    return this.http.put(this.baseUrl + 'users', member);
+  updateMember(member: Member) {
+    return this.http.put(this.baseUrl + 'users', member).pipe(
+      tap(() => {
+
+        this.members.update(members => members.map(m => m.userName === member.userName ? member : m));
+      })
+    );
   }
 
 }
