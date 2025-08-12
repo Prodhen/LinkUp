ea5a718e63340c7f1139542c725befc6252457ae
Prodhen <mdaroshprodhen8@gmail.com>
2025-08-12 11:55:00 +0600

    TestGit

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -71,7 +71,7 @@ namespace API.Controllers
                 return NoContent(); 
             }
 
-            return BadRequest("Failed to update the user due to internal error-test-git");
+            return BadRequest("Failed  to update the user due to internal error-test-git");
         }
 
     }
