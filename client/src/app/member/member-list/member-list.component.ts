1502802b3fd469e8bda7df1b3b0faf26c3529a97
Arosh <mdaroshprodhen8@gmail.com>
2025-09-11 09:18:54 +0600

    Using the angular bootstrap pagination

--- client/src/app/member/member-list/member-list.component.ts
+++ client/src/app/member/member-list/member-list.component.ts
@@ -3,11 +3,12 @@ import { MembersService } from '../../_services/members.service';
 import { Member } from '../../_models/member';
 import { MemberCardComponent } from '../../members/member-card/member-card.component';
 import { NgFor } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [MemberCardComponent, NgFor],
+  imports: [MemberCardComponent, NgFor, PaginationModule],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
@@ -24,6 +25,12 @@ export class MemberListComponent implements OnInit {
 
   loadMembers() {
     this.memberService.getMembers(this.pageNumber, this.pageSize);
+  }
+  pageChanged(event: any) {
+    if (this.pageNumber !== event.page) {
+      this.pageNumber = event.page;
+      this.loadMembers();
+    }
   }
 
 }
