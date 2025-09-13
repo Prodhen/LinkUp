47d7dfe1b83c19a16714fc00929115b958a695e1
Arosh <mdaroshprodhen8@gmail.com>
2025-09-13 10:35:53 +0600

    Formatting dates

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,10 +1,13 @@
 using System;
+using API.Helpers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
+
+[ServiceFilter(typeof(LogUserActivity))]
 [ApiController]
 [Route("api/[controller]")]
-public class BaseApiController:ControllerBase
+public class BaseApiController : ControllerBase
 {
 
 }
--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -5,6 +5,7 @@ import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr'
+import { TimeagoModule } from 'ngx-timeago';
 import { errorsInterceptor } from './_interceptors/errors.interceptor';
 import { jwtInterceptor } from './_interceptors/jwt.interceptor';
 import { NgxSpinnerModule } from 'ngx-spinner';
@@ -14,14 +15,15 @@ import { loadingInterceptor } from './_interceptors/loading.interceptor';
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }),
   provideRouter(routes),
-  provideHttpClient(withInterceptors([errorsInterceptor, jwtInterceptor,loadingInterceptor])),
+  provideHttpClient(withInterceptors([errorsInterceptor, jwtInterceptor, loadingInterceptor])),
   provideAnimations(),
   provideToastr({
     positionClass: 'toast-bottom-right'
   }),
   importProvidersFrom(NgxSpinnerModule.forRoot({
-      type: 'line-scale-party'
-    }))
-  
+    type: 'line-scale-party'
+  })),
+  importProvidersFrom(TimeagoModule.forRoot())
+
   ]
 };
