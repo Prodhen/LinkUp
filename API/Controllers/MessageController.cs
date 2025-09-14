d9c3e735639d8d720b9ab297488fa57368cf52e6
Arosh <mdaroshprodhen8@gmail.com>
2025-09-14 08:07:28 +0600

    Message controller setup

---
+++ API/Controllers/MessageController.cs
@@ -0,0 +1,104 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using API.Helpers;
+using API.Interface;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MessageController : BaseApiController
+    {
+        private readonly IMessageRepository messageRepository;
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+        public MessageController(IMessageRepository messageRepository, IUserRepository userRepository, IMapper mapper)
+        {
+            this.messageRepository = messageRepository;
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+        }
+        [HttpPost]
+        public async Task<ActionResult<MessageDto>> CreateMessage(AddMessageDto addMessageDto)
+        {
+            var username = User.GetUsername();
+
+            if (username == addMessageDto.RecipientUsername.ToLower())
+                return BadRequest("You cannot message yourself");
+
+            var sender = await userRepository.GetUserByUserNameAsync(username);
+            var recipient = await userRepository.GetUserByUserNameAsync(addMessageDto.RecipientUsername);
+
+            if (recipient == null || sender == null) return BadRequest("Cannot send message at this time");
+
+            var message = new Message
+            {
+                Sender = sender,
+                Recipient = recipient,
+                SenderUsername = sender.UserName,
+                RecipientUsername = recipient.UserName,
+                Content = addMessageDto.Content
+            };
+
+            messageRepository.AddMessage(message);
+
+            if (await messageRepository.SaveAllAsync()) return Ok(mapper.Map<MessageDto>(message));
+
+            return BadRequest("Failed to save message");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessagesForUser(
+            [FromQuery] MessageParams messageParams)
+        {
+            messageParams.Username = User.GetUsername();
+
+            var messages = await messageRepository.GetMessagesForUser(messageParams);
+
+            Response.AddPaginationHeader(messages);
+
+            return messages;
+        }
+
+        [HttpGet("thread/{username}")]
+        public async Task<ActionResult<IEnumerable<MessageDto>>> GetMessageThread(string username)
+        {
+            var currentUsername = User.GetUsername();
+
+            return Ok(await messageRepository.GetMessageThread(currentUsername, username));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteMessage(int id)
+        {
+            var username = User.GetUsername();
+
+            var message = await messageRepository.GetMessage(id);
+
+            if (message == null) return BadRequest("Cannot delete this message");
+
+            if (message.SenderUsername != username && message.RecipientUsername != username)
+                return Forbid();
+
+            if (message.SenderUsername == username) message.SenderDeleted = true;
+            if (message.RecipientUsername == username) message.RecipientDeleted = true;
+
+            if (message is { SenderDeleted: true, RecipientDeleted: true })
+            {
+                messageRepository.DeleteMessage(message);
+            }
+
+            if (await messageRepository.SaveAllAsync()) return Ok();
+
+            return BadRequest("Problem deleting the message");
+        }
+
+    }
+}
